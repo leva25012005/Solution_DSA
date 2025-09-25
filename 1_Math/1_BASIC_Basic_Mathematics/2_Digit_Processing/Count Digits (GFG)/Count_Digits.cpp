76867112302bf4adf5ff2c7c7692f7ec1696394d
leva25012005 <nhanzh1@gmail.com>
2025-09-25 09:13:46 +0700

    code(Count_Digits.cpp): rename function

--- 1_Math/1_BASIC_Basic_Mathematics/2_Digit_Processing/Count Digits (GFG)/Count_Digits.cpp
+++ 1_Math/1_BASIC_Basic_Mathematics/2_Digit_Processing/Count Digits (GFG)/Count_Digits.cpp
@@ -8,11 +8,12 @@ using namespace std;
 class Solution {
 public:
   // 1. Using string conversion
-  int countDigitsString(int n) { return to_string(n).length(); }
   // Time: O(log n), Space: O(log n) (string creation)
+  int Solution1(int n) { return to_string(n).length(); }
 
   // 2. Iterative division by 10
-  int countDigitsIterative(int n) {
+  // Time: O(log n), Space: O(1)
+  int Solution2(int n) {
     int count = 0;
     while (n > 0) {
       n /= 10;
@@ -20,22 +21,22 @@ public:
     }
     return count;
   }
-  // Time: O(log n), Space: O(1)
 
   // 3. Using logarithm
-  int countDigitsLog(int n) { return (int)log10(n) + 1; }
   // Time: O(1), Space: O(1)
+  int Solution3(int n) { return (int)log10(n) + 1; }
 
-  // 5. Recursive division
-  int countDigitsRecursive(int n) {
+  // 4. Recursive division
+  // Time: O(log n), Space: O(log n) (stack)
+  int Solution4(int n) {
     if (n < 10)
       return 1;
-    return 1 + countDigitsRecursive(n / 10);
+    return 1 + Solution4(n / 10);
   }
-  // Time: O(log n), Space: O(log n) (stack)
 
-  // 6. Functional / STL style
-  int countDigitsFunctional(int n) {
+  // 5. Functional / STL style
+  // Time: O(log n), Space: O(log n) (vector)
+  int Solution5(int n) {
     vector<int> digits;
     while (n > 0) {
       digits.push_back(n % 10);
@@ -43,18 +44,17 @@ public:
     }
     return digits.size();
   }
-  // Time: O(log n), Space: O(log n) (vector)
 };
 
 int main() {
   Solution sol;
   int n = 456;
 
-  cout << "String: " << sol.countDigitsString(n) << "\n";
-  cout << "Iterative: " << sol.countDigitsIterative(n) << "\n";
-  cout << "Logarithm: " << sol.countDigitsLog(n) << "\n";
-  cout << "Recursive: " << sol.countDigitsRecursive(n) << "\n";
-  cout << "Functional: " << sol.countDigitsFunctional(n) << "\n";
+  cout << "String: " << sol.Solution1(n) << "\n";
+  cout << "Iterative: " << sol.Solution2(n) << "\n";
+  cout << "Logarithm: " << sol.Solution3(n) << "\n";
+  cout << "Recursive: " << sol.Solution4(n) << "\n";
+  cout << "Functional: " << sol.Solution5(n) << "\n";
 
   return 0;
 }
